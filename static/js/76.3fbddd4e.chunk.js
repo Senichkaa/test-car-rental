23e571110d70a1f73389bb77a471aeafbc86c61e
Senichkaa <90269692+Senichkaa@users.noreply.github.com>
2024-01-17 16:45:52 +0000

    Deploying to gh-pages from @ Senichkaa/test-car-rental@f0d0f947e26c9f59f2c30c12ffb206a18ea9928f 🚀

--- static/js/76.3fbddd4e.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunktest_car_rental=self.webpackChunktest_car_rental||[]).push([[76],{76:function(e,t,n){n.r(t);n(791);var r=n(184);t.default=function(){return(0,r.jsx)("div",{children:"HomePage"})}}}]);
-//# sourceMappingURL=76.3fbddd4e.chunk.js.map
