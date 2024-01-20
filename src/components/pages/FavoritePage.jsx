b05925907e4a74eb67fa0cae5f81d21809c4a0d3
Senichkaa <super-kapanaiko406@ukr.net>
2024-01-20 22:58:45 +0200

    added filter and rendering to favorites

--- src/components/pages/FavoritePage.jsx
+++ src/components/pages/FavoritePage.jsx
@@ -1,9 +1,50 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { MainContainer } from 'common/MainContainer';
+import Filter from 'components/Filter/Filter';
+import Cars from 'components/Cars/Cars';
+
+import { fetchAllCars } from '../../redux/cars/carsThunk';
+import {
+  selectFavoriteFilters,
+  selectFavoriteFiltered,
+  selectIsFavoriteFiltered,
+} from '../../redux/favorite/favoriteSelectors';
+import {
+  setFavoriteFilters,
+  setFavoriteFiltered,
+  setIsFavoriteFiltered,
+} from '../../redux/favorite/favoriteSlice';
+import { selectAllCars } from '../../redux/cars/carsSelectors';
 
 const FavoritePage = () => {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchAllCars());
+  }, [dispatch]);
+  const favorite = useSelector(state => state.favorite.favorites);
+  const cars = useSelector(selectAllCars);
+  const favoriteCars = cars.filter(item => favorite.includes(item.id));
+  const isFavoriteFiltered = useSelector(selectIsFavoriteFiltered);
+  const favoriteFilters = useSelector(selectFavoriteFilters);
+  const filteredFavorite = useSelector(selectFavoriteFiltered);
   return (
-    <div>FavoritePage</div>
-  )
-}
+    <>
+      <MainContainer>
+        <section>
+          <Filter
+            cars={favoriteCars}
+            filters={favoriteFilters}
+            inFilter={value => dispatch(setFavoriteFiltered(value))}
+            isFiltered={isFavoriteFiltered}
+            changeIsFiltered={value => dispatch(setIsFavoriteFiltered(value))}
+            changeFilter={value => dispatch(setFavoriteFilters(value))}
+          />
+          <Cars cars={isFavoriteFiltered ? filteredFavorite : favoriteCars} />
+        </section>
+      </MainContainer>
+    </>
+  );
+};
 
-export default FavoritePage
+export default FavoritePage;
--- src/redux/favorite/favoriteSelectors.js
+++ src/redux/favorite/favoriteSelectors.js
@@ -1,4 +1,5 @@
 export const selectFavorites = state => state.favorite.favorites;
 export const selectFavoriteFilters = state => state.favorite.favoriteFilters;
-export const selectIsFavoriteFiltered = state => state.favorite.isFavoriteFiltered;
+export const selectIsFavoriteFiltered = state =>
+  state.favorite.isFavoriteFiltered;
 export const selectFavoriteFiltered = state => state.favorite.favoriteFiltered;
